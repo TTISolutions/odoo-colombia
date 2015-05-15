5fe013870d9a516de44cf248eb0376395ed59b0f
ccarly <ccarly@gmail.com>
2015-05-15 01:36:21 -0500

    l10n_co_base_country

---
+++ l10n_co_base_country/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "l10n_co_base_country",
+
+    'summary': """
+        Códigos dian para paises""",
+
+    'description': """
+    """,
+
+    'author': "FONSE SAS",
+    'website': "http://www.fonse.net",
+    'category': 'Localization',
+    'version': '0.1',
+    'depends': ['base'],
+    'data': [
+        'res_country_view.xml',
+        'res_country_data.xml',
+    ],
+    'installable': True,
+    'auto_install': False,    
+}
