9dac636e3f7648c9a585a88a6d71c3e04ad127e6
ccarly <ccarly@gmail.com>
2015-05-15 02:23:55 -0500

    Modify partner

--- l10n_co_base_partner/__openerp__.py
+++ l10n_co_base_partner/__openerp__.py
@@ -1,34 +1,20 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "l10n_co_base_partner",
+    'name': "Partner with names",
 
     'summary': """
-        l10n_co_base_partner""",
+        Partner with first and last second names""",
 
     'description': """
-        l10n_co_base_partner
     """,
-
     'author': "FONSE SAS",
     'website': "http://www.fonse.net",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
-    # for the full list
     'category': 'Extra Tools',
     'version': '0.1',
-
-    # any module necessary for this one to work correctly
     'depends': ['base'],
-
-    # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
         'res_users_view.xml',
         'res_partner_view.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [ 
     ],
     'installable': True,
     'auto_install': False,    
